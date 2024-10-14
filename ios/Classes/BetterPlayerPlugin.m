27d0859a343bdae0a04bde45a7c4394b52dd1d1b
Bill Mau Wai Piu <billmau@hket.com>
2024-10-14 15:25:17 +0800

    test pip data

--- ios/Classes/BetterPlayerPlugin.m
+++ ios/Classes/BetterPlayerPlugin.m
@@ -399,10 +399,14 @@ bool _remoteCommandsInitialized = false;
             [player setTrackParameters:width: height : bitrate];
             result(nil);
         } else if ([@"enablePictureInPicture" isEqualToString:call.method]){
-            double left = [argsMap[@"left"] doubleValue];
-            double top = [argsMap[@"top"] doubleValue];
-            double width = [argsMap[@"width"] doubleValue];
-            double height = [argsMap[@"height"] doubleValue];
+            double left = 0;
+            double top = 99;
+            double width = 160;
+            double height = 90;
+            // double left = [argsMap[@"left"] doubleValue];
+            // double top = [argsMap[@"top"] doubleValue];
+            // double width = [argsMap[@"width"] doubleValue];
+            // double height = [argsMap[@"height"] doubleValue];
             [player enablePictureInPicture:CGRectMake(left, top, width, height)];
         } else if ([@"isPictureInPictureSupported" isEqualToString:call.method]){
             if (@available(iOS 9.0, *)){
